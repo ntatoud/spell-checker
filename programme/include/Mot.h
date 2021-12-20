68d85bade8da0595c0bc7682c911f6b0d22aee72
loick toupin <loick.toupin@insa-rouen.fr>
2021-12-20 21:00:01 +0100

    mot.h et .c, .c à compléter encore

--- programme/include/Mot.h
+++ programme/include/Mot.h
@@ -1,17 +1,26 @@
 #ifndef __MOT__
 #define __MOT__
+#include "EnsembleDeMot.h"
+#include <wchar.h>
 
 typedef struct Mot{
-  char* chaine; 
+  wchar_t * chaine; 
   unsigned int longueur; 
 } Mot;
+                                    //exemple : wchar_t p = L"Hello" ;
+int M_estUnMotValide(wchar_t *);
+Mot M_creerUnMot(wchar_t *);
+unsigned int M_longueurMot(Mot);
+wchar_t M_iemeCaractere(Mot, unsigned int);
+int M_sontIdentiques(Mot, Mot);
+Mot M_fixerIemeCaractere(Mot, unsigned int, wchar_t);
+Mot M_fixerLongueur(Mot, unsigned int);
 
-int M_estUnMotValide(char c);
-Mot M_creerUnMot(char c);
-unsigned int M_longueurMot(Mot mot);
-char M_iemeCaractere(Mot mot, unsigned int i);
-int M_sontIdentiques(Mot mot1, Mot mot2);
-void M_fixerIemeCaractere(Mot*, unsigned int, char);
-void M_fixerLongueur(Mot*, unsigned int);
+Mot M_supprimerIemeLettre(Mot, unsigned int );
+Mot M_inverserDeuxLettresConsecutives(Mot, unsigned int );
+Mot M_insererLettre(Mot, unsigned int, wchar_t);
+Mot M_decomposerMot(Mot*, unsigned int); //on modifie le mot et récupère le mot de sortie
+Mot M_reduireLaCasse(Mot);
+
 
 #endif
--- programme/src/Mot.c
+++ programme/src/Mot.c
@@ -1,36 +1,76 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <string.h>
+#include <wchar.h>
 #include "Mot.h"
 
-int M_estUnMotValide(char c){
-    Mot unMot;
-    unMot.longueur=1;
-    return 1;
+int M_estUnMotValide(wchar_t * c){
+    int longueurChaine = wcslen(c);
+    int estValide = 1;
+    int i = 1;
+    while(estValide && i<=longueurChaine){ //la cd utilise acceder ieme car..
+        /*
+        valide=E_estPresent(CaractereAlphabetique,c[i])
+        */
+    }
+    return estValide;
+}
+/* versoin cd : est-ce qu'on prend un mot en entrée ou une wcah*
+
+int M_estUnMotValide(wchar_t * c){
+    int valide = 1;
+    int i = 1;
+    while((valide) && (i<=M_longueur(mot))){
+        c=M_iemeCaractere(mot,i);
+        valide=E_estPresent(CaractereAlphabetique,c)
+        i=i+1
+    }
+    return valide;
 }
 
-Mot M_creerUnMot(char c){
+
+
+*/
+
+
+Mot M_creerUnMot(wchar_t * c){
     Mot unMot;
-    unMot.longueur=1;
+    unMot.longueur=wcslen (c);
+    unMot.chaine=c;
     return unMot;
 }
 
 unsigned int M_longueurMot(Mot mot){
-    return 1;
+    return mot.longueur;
 }
 
-char M_iemeCaractere(Mot mot, unsigned int i){
-    char c="a";
-    return c;
+wchar_t M_iemeCaractere(Mot mot, unsigned int i){
+    return mot.chaine[i];
 }
 
 int M_sontIdentiques(Mot mot1, Mot mot2){
     return 1;
 }
 
-void M_fixerIemeCaractere(Mot *unMot, unsigned int i, char c){
-    ;
+Mot M_fixerIemeCaractere(Mot unMot, unsigned int i, wchar_t c){
+    return unMot;
 }
-void M_fixerLongueur(Mot *unMot, unsigned int i){
-    ;
+Mot M_fixerLongueur(Mot unMot, unsigned int i){
+    return unMot;
+}
+
+Mot M_supprimerIemeLettre(Mot unMot, unsigned int i){
+    return unMot;
+}
+Mot M_inverserDeuxLettresConsecutives(Mot unMot, unsigned int i){
+    return unMot;
+}
+Mot M_insererLettre(Mot unMot, unsigned int i, wchar_t c){
+    return unMot;
+}
+Mot M_decomposerMot(Mot* unMot, unsigned int i){ //on modifie le mot et récupère le mot de sortie
+    return *unMot;
+} 
+Mot M_reduireLaCasse(Mot unMot){
+    return unMot;
 }
