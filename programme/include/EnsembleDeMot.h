019550c02ad0c7367fb1d1d4e634a4fdfae2eee2
Tatoud Noé <noe.tatoud@insa-rouen.fr>
2021-12-31 14:29:23 +0100

    corrections indentations des fichiers EDM.c EDM.h et testEDM.c plus maj testEDM.c sans fuites mémoires

--- programme/include/EnsembleDeMot.h
+++ programme/include/EnsembleDeMot.h
@@ -6,27 +6,22 @@
 
 /* Partie privee */
 typedef struct EnsembleDeMot{
-  ListeChaineeDeMot lesMots;
-  long int nbMots;
+    ListeChaineeDeMot lesMots;
+    long int nbMots;
 } EnsembleDeMot;
-
 
 /* Partie publique */
 #define EDM_ERREUR_MEMOIRE 1
 
 EnsembleDeMot ensembleDeMot();
-void EDM_vider(EnsembleDeMot*); 
-EnsembleDeMot EDM_copier(EnsembleDeMot);  /* errno=EDM_ERREUR_MEMOIRE si pas assez de m�moire */
-int EDM_egale(EnsembleDeMot,EnsembleDeMot);
-void EDM_ajouter(EnsembleDeMot*,Mot);  /* errno=EDM_ERREUR_MEMOIRE si pas assez de m�moire */
-void EDM_retirer(EnsembleDeMot*,Mot);
-int EDM_estPresent(EnsembleDeMot,Mot);
+void EDM_vider(EnsembleDeMot *);
+EnsembleDeMot EDM_copier(EnsembleDeMot); /* errno=EDM_ERREUR_MEMOIRE si pas assez de m�moire */
+int EDM_egale(EnsembleDeMot, EnsembleDeMot);
+void EDM_ajouter(EnsembleDeMot *, Mot); /* errno=EDM_ERREUR_MEMOIRE si pas assez de m�moire */
+void EDM_retirer(EnsembleDeMot *, Mot);
+int EDM_estPresent(EnsembleDeMot, Mot);
 long int EDM_cardinalite(EnsembleDeMot);
 
-EnsembleDeMot EDM_union(EnsembleDeMot,EnsembleDeMot);  /* errno=EDM_ERREUR_MEMOIRE si pas assez de m�moire */
-EnsembleDeMot EDM_intersection(EnsembleDeMot,EnsembleDeMot);  /* errno=EDM_ERREUR_MEMOIRE si pas assez de m�moire */
-EnsembleDeMot EDM_soustraction(EnsembleDeMot,EnsembleDeMot);  /* errno=EDM_ERREUR_MEMOIRE si pas assez de m�moire */
-
-
+EnsembleDeMot EDM_union(EnsembleDeMot, EnsembleDeMot);        /* errno=EDM_ERREUR_MEMOIRE si pas assez de m�moire */
 Mot EDM_obtenirMot(EnsembleDeMot);
-#endif 
+#endif
