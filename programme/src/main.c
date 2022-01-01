855cc9b6d5640afdb40146f40746ecd66c6b071a
Tatoud Noé <noe.tatoud@insa-rouen.fr>
2022-01-01 12:10:05 +0100

    Ajout du programme principal

--- programme/src/main.c
+++ programme/src/main.c
@@ -1,7 +1,25 @@
 #include <stdlib.h>
-#include <assert.h>
+#include <stdio.h>
 #include <string.h>
+#include "IHM.h"
 
-int main(){
-    return 0;
+
+int main(int argc, char** argv){
+
+    if(argc <= 2 || argc > 5){
+        if(argc == 2)
+            gererCommandeAide(argv[1]);
+        else 
+            gererCommandeAide("");
+    }
+   
+    else if(argc == 3){
+        gererCommandeCorrection(argv[1], argv[2]);
+    }   
+
+    else if(argc == 5){
+        gererCommandeCreerDico(argv[1], argv[2], argv[3], argv[4]);
+    }
+
+    return EXIT_SUCCESS;
 }
